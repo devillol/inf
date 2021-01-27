09cce708cb4d93bb197a3b94235b21978bef0b0c
Galya <galina.bosinzon@raiffeisen.ru>
2021-01-27 13:17:37 +0300

    push

--- 3.4.py
+++ 3.4.py
@@ -1,9 +1,11 @@
 a = input("Введите массив: ")
-max, ind = -1, []
+
 for n, i in enumerate(a.split()):
-    if int(i) > max:
-        max, ind = int(i), [n + 1]
-    elif int(i) == max:
+    if n == 0:
+        max, ind = i, []
+    if float(i) > float(max):
+        max, ind = i, [n + 1]
+    elif float(i) == float(max):
         ind.append(n + 1)
 
 print(f"Максимальный элемент {', '.join([f'A[{i}]' for i in ind])} = {max}")
